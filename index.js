a045b46db4094402489ee6aa71741327c69ab290
Brian <briantcheung@gmail.com>
2021-03-02 17:04:55 -0800

    commit tree 06b2bd7cbd1b146e9863652defff4eb354346903

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Add your code here
+function submitData(username, email) {
+    let formData = {
+        name: username,
+        email: email
+    };
+
+    let configObj = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify(formData)
+    };
+
+    return fetch("http://localhost:3000/users", configObj)
+        .then(res => res.json())
+        .then(obj => addIdToPage(obj.id))
+        .catch(error => throwError(error));
+}
+
+function addIdToPage(id) {
+    let newId = document.createElement("p");
+    newId.textContent = id;
+    document.querySelector("body").appendChild(newId);
+}
+
+function throwError(error) {
+    let errorMsg = document.createElement("p");
+    errorMsg.textContent = error;
+    document.querySelector("body").appendChild(errorMsg);
+}
